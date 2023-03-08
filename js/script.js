f5a51ad8e33cfe8440efeb626c08618d7a85ae76
Lip-7 <zhighofficial@gmail.com>
2023-03-08 17:02:04 +0100

    fixed more punctuation

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ function priceCalculator(){
         price *= 0.8;
     }else if (age > 65){
         price *= 0.6;
-    };
+    }
     if ((age != 0) && (km != 0)){
         document.getElementById('ticketMessage').innerHTML = `<h2 id="ticketMessage" class="h1 m-5">The ticket cost is <span id="ticketCosttry">${price.toFixed(2)}</span> &euro;</h2>`;
     }else{
         document.getElementById('ticketMessage').innerHTML = `<h2 id="ticketMessage" class="h1 m-5">${errorMessage} <span id="ticketCosttry">0</span> &euro;</h2>`;
-    };
+    }
     document.getElementById('result').classList.remove('d-none');
     document.getElementById('calculator').classList.add('d-none');
 }
